13fcb2f3f5ea5609e136aa42d5c8067e7cd99d1c
Shane Dosch <shanedosch@gmail.com>
2016-12-05 15:28:37 -0800

    Setup repo for deployment in Bitbucket Pipelines

---
+++ commit_to_git_and_upload_to_s3.sh
@@ -0,0 +1,10 @@
+cd /home/shane_d/voters_info/votersedge-voting-info &&
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "*** Files have been updated. ***";
+  git add *
+  git commit -m "latest voter info updates"
+  git push origin master
+  aws s3 cp /home/shane_d/voters_info/votersedge-voting-info/json s3://votersedge-voting-info-static/json --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive --include "*.*"
+else 
+  echo "*** Files have not changed. Nothing to commit and upload. ***";
+fi
